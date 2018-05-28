a6a4bad17971a615f2f8d635f07cfe5091790296
Sun Xiaofei <adoni1203@gmail.com>
2018-05-28 21:33:21 +0800

    Add some basic model

---
+++ language_model/language_model.py
@@ -0,0 +1,37 @@
+# encoding: utf-8
+"""
+    language_model.py
+    ~~~~~~~~~
+    
+    Model description
+    
+    :author: Xiaofei Sun
+    :contact: adoni1203@gmail.com
+    :date: 2018/5/28
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+class LanguageModel(object):
+    """
+    General language model class
+    """
+
+    def __init__(self, model_file):
+        pass
+
+    def predict_next_word(self, prefix):
+        """
+        :param prefix:
+        :return: a word which is most possible to be the next word of this prefix
+        """
+        pass
+
+    def predict_sentence(self, prefix):
+        pass
+
+    def predict_middle_word(self, prefix, postfix):
+        pass
+
+    def scoring(self, sentence):
+        pass
